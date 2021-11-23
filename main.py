2eaa3b269b092c04fcee8ac0438742092b3633a8
c-phrase <michael.minock@gmail.com>
2021-11-23 14:25:40 +0100

    first nltk

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import nltk
 from xml.dom import minidom
 
-dom = minidom.parse("examples.xml")
+dom = minidom.parse("train.xml")
 
-for qtag in dom.getElementsByTagName("question"):
-    print(qtag.getAttribute("text"))
+for qtag in dom.getElementsByTagName("utterance"):
+    if qtag.getAttribute("sensical") == "true":
+        print(f"{qtag.getAttribute('text')} makes sense")
+    else:
+        print(f"{qtag.getAttribute('text')} is weird")
