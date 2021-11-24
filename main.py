429eda9d1638dec2e0afeb25679d14986a711351
Alfons Edbom <alfons98@live.com>
2021-11-24 22:06:07 +0100

    Redid some stuff

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import nltk
+from xml.dom import minidom
 
+dom = minidom.parse("train.xml")
 
-print('weeble')
+for qtag in dom.getElementsByTagName("utterance"):
+    if qtag.getAttribute("sensical") == "true":
+        print(f"{qtag.getAttribute('text')} makes sense")
+    else:
+        print(f"{qtag.getAttribute('text')} is weird")
