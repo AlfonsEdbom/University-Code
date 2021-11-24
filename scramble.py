024831b6322ac5347f16d14dec9b72f1f071077d
Alfons Edbom <alfons98@live.com>
2021-11-24 22:01:38 +0100

    Remade formatting.

--- scramble.py
+++ scramble.py
@@ -1,5 +1,5 @@
 import random
-import nltk
+# import nltk
 from nltk.tokenize import sent_tokenize
 
 
@@ -10,32 +10,25 @@ def scramble(inputfile, outputfile="MyFileScrambeled.txt"):
     with open(inputfile, 'r') as fin:  # reads a file and creates string
         line_list = fin.readlines()
         for line in line_list:
-            formated_line = line.replace("\n", " ")
-            my_string += formated_line
+            if line == "\n":  # handles newline rows
+                continue
+            else:
+                formated_line = line.replace("\n", " ")
+                my_string += formated_line
 
     tokens = sent_tokenize(my_string)  # create list containing each sentence
 
     for sentence in tokens:  # go through all sentences and scramble
-        sentence_list = sentence.split(' ')
+        sentence_list = sentence[:-1].split(' ', )  # -1 to remove '.'
         random.shuffle(sentence_list)
-        [scrambled_list.append(word) for word in sentence_list]
+        sentence_string = ' '.join(sentence_list).strip().capitalize()
+        sentence_string += '.'
+        scrambled_list.append(sentence_string)
 
-    # Some very ugly formatting for it be readable.
-    if scrambled_list[0].islower():
-        scrambled_list[0] = scrambled_list[0].capitalize()
-        print(scrambled_list[0])
-
-    for i in range(1, len(scrambled_list)):
-        if scrambled_list[i - 1][-1] == '.':
-            scrambled_list[i] = scrambled_list[i].capitalize()
-
-        if scrambled_list[i].isupper():
-            if scrambled_list[i - 1][-1] != '.':
-                scrambled_list[i] = scrambled_list[i][0].lower()
 
     with open(outputfile, 'w') as fout:
         for line in scrambled_list:
-            fout.write(line + ' ')
+            fout.write(line + '\n')
 
 
 if __name__ == '__main__':
