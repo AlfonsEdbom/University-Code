8781394cef68490da59c9ddecf556c66bf6e9cee
Alfons Edbom <alfons98@live.com>
2021-11-24 22:03:56 +0100

    Added some bad sentences in test and train.

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 import nltk
-from xml.dom import minidom
 
-dom = minidom.parse("train.xml")
 
-for qtag in dom.getElementsByTagName("utterance"):
-    if qtag.getAttribute("sensical") == "true":
-        print(f"{qtag.getAttribute('text')} makes sense")
-    else:
-        print(f"{qtag.getAttribute('text')} is weird")
+print('weeble')
