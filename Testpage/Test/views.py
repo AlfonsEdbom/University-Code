62f6e9a3a858496a7e91db9ebb4396127c22fd80
AlfonsEdbom <Alfons98@live.com>
2021-12-22 19:35:13 +0100

    Added /Test/upload path to upload and save a file in a folder "media" and display path name

--- Testpage/Test/views.py
+++ Testpage/Test/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.core.files.storage import FileSystemStorage
 
 
 def index(request):
     template = loader.get_template('index.html')
 
     return render(request, 'index.html')
+
+def upload(request):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+    return render(request, 'upload.html', context)
 
--- Testpage/Testpage/urls.py
+++ Testpage/Testpage/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+ #   path('upload/', views.upload, name='upload'),
     path('Test', include('Test.urls')),
     path('admin/', admin.site.urls),
 ]
