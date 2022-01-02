05eb0d5c36cc1025294128045b9e2111c9a57b47
AlfonsEdbom <Alfons98@live.com>
2022-01-02 17:11:03 +0100

    /predict is now working pretty good

--- Testpage/Test/urls.py
+++ Testpage/Test/urls.py
@@ -6,10 +6,10 @@ from . import views
 
 urlpatterns = [
     path('test', views.test, name='test'),
-    path('predict', views.predict_text, name='predict'),
+    path('predict', views.predict_sentence, name='predict'),
     path('upload', views.upload, name='upload'), #TODO: Potentially not needed if not uploading train/test data
     path('', views.index, name='index'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #TODO: Same as above
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #TODO: Same as at upload
--- Testpage/Test/views.py
+++ Testpage/Test/views.py
@@ -17,6 +17,21 @@ def index(request):
     return render(request, 'index.html')
 
 
+def predict_sentence(request):
+    context = {}
+    if request.method == 'POST':
+        form = TextForm(request.POST)  # If a file is uploaded request.FILES is needed also
+        if form.is_valid():
+            t = form.cleaned_data["text"]
+            context["text"] = t
+            prediction = predict(t)
+            context["prediction"] = prediction
+    else:
+        render(request, 'predict.html')
+
+    return render(request, 'predict.html', context)
+
+
 def upload(request):
     context = {}
     if request.method == 'POST':
@@ -26,9 +41,11 @@ def upload(request):
         context['url'] = fs.url(name)
     return render(request, 'upload.html', context)
 
+
 def test(request):
+    #this is not needed right now, still here for testing things
     context = {}
-    if request.method =='POST':
+    if request.method == 'POST':
         form = TextForm(request.POST)
         if form.is_valid():
             t = form.cleaned_data["text"]
@@ -39,34 +56,3 @@ def test(request):
         render(request, 'test.html')
 
     return render(request, 'test.html', context)
-
-def predict_text(request):
-    context = {}
-    if request.method == "POST":
-        form = TextForm(request.POST) #If a file is uploaded request.FILES is needed also
-        #if form.is_valid():
-            #text_string = form.cleaned_data["text"]
-            #t = PredictString(text=text_string)
-            #t.save()
-
-            #result = predict(["This", "is", "a", "temporary", "prediction", "list"]) #TODO: Add the right string here
-            #resp = {'data': "maybe t", 'result': result} #TODO: Figure out what should be here
-            #return render(request, "Test/result.html", resp)
-    else:
-        pass
-
-    return render(request, 'predict.html', context)
-def predict_text_old(request):
-    context = {}
-    if request.method == "POST":
-        form = TextForm(request.POST) #TODO: If a file is uploaded request.FILES is needed also
-        if form.is_valid():
-            text_string = form.cleaned_data["text"]
-            t = PredictString(text=text_string)
-            t.save()
-
-            result = predict(["This", "is", "a", "temporary", "prediction", "list"]) #TODO: Add the right string here
-            resp = {'data': "maybe t", 'result': result} #TODO: Figure out what should be here
-            return render(request, "Test/result.html", resp)
-    else:
-        return render(request, "Test/predict.html")
