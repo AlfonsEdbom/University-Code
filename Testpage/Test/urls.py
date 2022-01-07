57b8533e819a05d321c5f6ceb9caa00c8e518d03
Loed0013 <94440181+Loed0013@users.noreply.github.com>
2022-01-07 14:18:16 +0100

    Bigger input files and removed unnececary html files

--- Testpage/Test/urls.py
+++ Testpage/Test/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('test', views.test, name='test'),
     path('predict', views.predict_sentence, name='predict'),
     path('upload', views.upload, name='upload'), #TODO: Potentially not needed if not uploading train/test data
     path('', views.index, name='index'),
