a6926375c4e7e68bdf7b1f325865cbbecebcb49c
Johan Lehto <93596859+SvartKaffe@users.noreply.github.com>
2022-01-11 11:55:13 +0100

    modifications

--- Learning/RNN_model.py
+++ Learning/RNN_model.py
@@ -20,7 +20,7 @@ def model_preprocessing(data, labels):
 
     # separate data into train/test/validation data/labels
     train_cutoff = int(len(data) * 0.75)  # how much
-    test_cutoff = int(len(data) * 1)
+    test_cutoff = int(len(data) * 0.85)
 
     train_data = np.asarray(shuffled_data[0: train_cutoff])  # from 0 to train_cutoff
     train_labels = np.asarray(shuffled_labels[0: train_cutoff])
@@ -28,11 +28,11 @@ def model_preprocessing(data, labels):
     test_data = np.asarray(shuffled_data[train_cutoff: test_cutoff])  # from train_cutoff to test_cutoff
     test_labels = np.asarray(shuffled_labels[train_cutoff: test_cutoff])
 
-    validation_data = np.asarray(shuffled_data[train_cutoff: -1])  # from test_cutoff to end
-    validation_labels = np.asarray(shuffled_labels[train_cutoff: -1])
+    validation_data = np.asarray(shuffled_data[test_cutoff: -1])  # from test_cutoff to end
+    validation_labels = np.asarray(shuffled_labels[test_cutoff: -1])
 
     # create word to int dict and reverse
-    VOCAB_SIZE = 1000
+    VOCAB_SIZE = 3000
     encoder = tf.keras.layers.TextVectorization(max_tokens=VOCAB_SIZE)
     encoder.adapt(train_data)
     vocab = np.array(encoder.get_vocabulary())
@@ -47,12 +47,12 @@ def create_RNN_model(train_data, train_labels, test_data, test_labels, validatio
         tf.keras.layers.Embedding(
             input_dim=len(encoder.get_vocabulary()),
             output_dim=64,
-            mask_zero=True),
+            ),
         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences=True)),
         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
         tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(1)])
+        tf.keras.layers.Dense(1, activation='sigmoid')])
 
     model.summary()
 
@@ -65,21 +65,21 @@ def create_RNN_model(train_data, train_labels, test_data, test_labels, validatio
 
     history = model.fit(train_data,
                         train_labels,
-                        epochs=10,
+                        epochs=14,
                         batch_size=64,
                         validation_data=(validation_data, validation_labels),
                         verbose=1)
 
     results = model.evaluate(test_data, test_labels)
 
-    #model.save('NLU_RNN_model/') #needed to export model
+    tf.keras.models.save_model(model, 'NLU_RNN_model/') #needed to export model
 
     return results, history
 
 
 if __name__ == "__main__":
     data_dir = os.path.join(os.path.dirname(__file__), 'data')
-    data_file = 'web_full.xml'
+    data_file = 'brown_full.xml'
     data_path = os.path.join(data_dir, data_file)
 
     data, labels = get_data(data_path)
