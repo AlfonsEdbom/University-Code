b5ae0f080970d50a021e8fb3f0bdf9adfd58d26e
Alfons Edbom <alfons98@live.com>
2022-01-13 11:03:44 +0100

    Website test

--- Test/webpage/urls.py
+++ Test/webpage/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('predict', views.predict_sentence, name='predict'),
