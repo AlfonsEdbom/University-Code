36254f2c964ecb4879c16bb7f533a1094438ca15
Loed0013 <94440181+Loed0013@users.noreply.github.com>
2022-01-13 09:57:19 +0100

    Test

---
+++ Test/webpage/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.template import loader
+from django.core.files.storage import FileSystemStorage
+from .forms import TextForm
+import os
+import tensorflow as tf
+
+global my_model
+
+model_dir = 'RNN_model'
+current_dir = os.path.dirname(__file__)
+model_path = os.path.join(current_dir, model_dir)
+
+my_model = tf.keras.models.load_model(model_path)
+
+# Create your views here.
+def index(request):
+    #template = loader.get_template('index.html')
+
+    return render(request, 'index.html')
+
+
+def predict_sentence(request):
+    global my_model
+    context = {}
+    if request.method == 'POST':
+        form = TextForm(request.POST)  # If a file is uploaded request.FILES is needed also
+        if form.is_valid():
+            t = form.cleaned_data["text"]
+            context["text"] = t
+            text_list = [t]
+            prediction = my_model.predict(text_list)
+            context["prediction"] = prediction
+    else:
+        render(request, 'predict.html')
+
+    return render(request, 'predict.html', context)
+
