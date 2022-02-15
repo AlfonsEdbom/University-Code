0709adbdcec100aa2ebe5b7fa1249eca3c742853
Alfons Edbom <alfons98@live.com>
2022-02-15 17:16:01 +0100

    Added kind of working 2 state and 4 state HMMs, still need to optimize starting positions

--- Exercises 4-5/Ex4_1.R
+++ Exercises 4-5/Ex4_1.R
@@ -1,20 +1,45 @@
 require(pacman)  # Gives a confirmation message.
-pacman::p_load(pacman, here, Biostrings, HMM)
+pacman::p_load(pacman, here, Biostrings, HMM, seqinr)
 
 #Get the path to the FASTA file
 virus_file <- here("sequences", "NC_001416.fasta")
 
 #load the file
-virus_seq <- read.fasta(virus_file)
+virus_seq <- read.fasta(virus_file,forceDNAtolower = FALSE)
 
+#Define parameters for HMM
+States <- c("H_GC", "H_AT")
+Symbols <- c("A", "T", "C", "G")
+start <- c(.5, .5)
+trans <- matrix(c(.75, .25, .25, .75), 2)
+emission <- matrix(c(.05, .45, .05, .45, .45, .05, .45, .05), 2)
 
-HMM_2state <- initHMM(c("H_GC", "H_AT"), c("A", "T", "C", "G"), )
+#Create 2 state HMM
+HMM_2state <- initHMM(States, Symbols, startProbs = start, transProbs = trans, emissionProbs = emission)
 
+#Simulate sequence of length 100 with HMM
 simHMM(HMM_2state, 100)
 
-viterbiTraining(HMM_2state, obs)
+#Train HMM to find optimal parameter settings
+obs <- unlist(getSequence(virus_seq))
+HMM_2new <- baumWelch(HMM_2state, obs, 10, delta=1E-9)
 
-obs <- toString(virus_seq)
+#Create 4 state HMM
+States <- c("H_G", "H_C","H_A", "H_T")
+Symbols <- c("G", "C", "A", "T")
+start <- c(.25, .25, .25, .25)
+trans <- matrix(c(.8, .1, .05, .05, .1, .8, .05, .05, .05, .05, .8, .1, .05, .05, .1, .8), 4)
+emission <- matrix(c(.8, .1, .05, .05, .1, .8, .05, .05, .05, .05, .8, .1, .05, .05, .1, .8), 4)
+
+#Create HNMM
+HMM_4state <- initHMM(States, Symbols, startProbs = start, transProbs = trans, emissionProbs = emission)
+
+#Simulate sequence of length 100 with HMM
+simHMM(HMM_4state, 100)
+
+#Train HMM to find optimal parameter settings
+obs <- unlist(getSequence(virus_seq))
+HMM_4new <- baumWelch(HMM_4state, obs, 20, delta=1E-9)
 
 
 
