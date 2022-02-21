19e0898bc4246d12f631d17f9bce99dd1dc7cfee
Alfons Edbom <alfons98@live.com>
2022-02-21 17:59:21 +0100

    Random bad stuff

--- connect_test.py
+++ connect_test.py
@@ -1,22 +1,45 @@
 import psycopg2
-
-print("Connecting to database")
-
-connection = psycopg2.connect(host="localhost",
- dbname="postdata",
-  user="postgres",
-   password="biotek18")
-
-my_cursor = connection.cursor()
-
-my_cursor.execute('SELECT category_name FROM categories;')
-
-records = my_cursor.fetchall()
-print(records)
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
-my_cursor.close()
+def connect_to_database(host, user, password):
+   con = psycopg2.connect(host=host, user=user, password=password, database="lab2")
+   con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-connection.close()
+   return con
 
-print("No longer connected to database")
+def create_database(db_name):
+   sqlCommand = f"CREATE DATABASE {db_name};"
+
+   return sqlCommand
+
+
+def create_table(table_name):
+
+   sqlCommand = f"""CREATE TABLE {table_name} (
+      name TEXT PRIMARY KEY,
+      amount INTEGER,
+      cost INTEGER);"""
+   return sqlCommand
+
+
+
+
+
+
+if __name__ == "__main__":
+   con = connect_to_database("localhost", "postgres", "biotek18")
+   cur = con.cursor()
+
+   #db_command = create_database("lab2")
+   #cur.execute(db_command)
+
+   #table_command = create_table("Snacks")
+   #cur.execute(table_command)
+
+   
+
+   cur.close()
+   con.close()
+
+   print("No longer connected to the database")
