e36d6a9c49160419eb0b64e9dba0bb15fda0ea71
Alfons Edbom <alfons98@live.com>
2022-03-01 15:45:45 +0100

    Finalized Ex41 added file for Ex51

--- Exercises 4-5/Ex4_1.R
+++ Exercises 4-5/Ex4_1.R
@@ -1,20 +1,23 @@
 require(pacman)  # Gives a confirmation message.
-pacman::p_load(pacman, here, Biostrings, HMM, seqinr)
+pacman::p_load(pacman, here, HMM, seqinr)
 
-#Get the path to the FASTA file
+#Get path to the FASTA file
 virus_file <- here("sequences", "NC_001416.fasta")
 
 #load the file
 virus_seq <- read.fasta(virus_file,forceDNAtolower = FALSE)
 
-#Define parameters for HMM
-States <- c("H_GC", "H_AT")
-Symbols <- c("A", "T", "C", "G")
-start <- c(.5, .5)
-trans <- matrix(c(.75, .25, .25, .75), 2)
-emission <- matrix(c(.05, .45, .05, .45, .45, .05, .45, .05), 2)
+#The states
 
-#Create 2 state HMM
+Symbols <- c("A", "T", "C", "G")
+
+#Define parameters for 2 state - HMM
+States <- c("H_GC", "H_AT")
+start <- c(.5, .5) #probability to start in either state
+trans <- matrix(c(.75, .25, .25, .75), 2) #probability to transition from a state to another
+emission <- matrix(c(.05, .45, .05, .45, .45, .05, .45, .05), 2) #for each state, the probability to emit a given symbol
+
+#Create 2 initial state HMM
 HMM_2state <- initHMM(States, Symbols, startProbs = start, transProbs = trans, emissionProbs = emission)
 
 #Simulate sequence of length 100 with HMM
@@ -39,13 +42,6 @@ simHMM(HMM_4state, 100)
 
 #Train HMM to find optimal parameter settings
 obs <- unlist(getSequence(virus_seq))
-HMM_4new <- baumWelch(HMM_4state, obs, 20, delta=1E-9)
+HMM_4new <- baumWelch(HMM_4state, obs, 10, delta=1E-9)
 
-
-
-#Close packages and plots
-p_unload(all) 
-detach("package:datasets", unload = TRUE) 
-
-# Clear console
-cat("\014")  # ctrl+L
+mo
