dc63b68c903d4178075335a492db9bd6e8308bcc
AlfonsEdbom <Alfons98@live.com>
2022-05-06 10:53:22 +0200

    Some updates

--- Filters.py
+++ Filters.py
@@ -1,23 +1,34 @@
+import re
+
+
 def calc_anneal_temp(sequence: str) -> float:
     anneal_temp = 0
 
     for base in sequence:
         if base in ["A", "T"]:
             anneal_temp += 2
-        elif base in ["C", "G"]: # Needs to be elif, since other char are gaps
+        elif base in ["C", "G"]:  # Needs to be elif, since other char are gaps
             anneal_temp += 4
-
 
     return anneal_temp
 
-def get_GCC(sequence: str, max_GC: int, min_GC: int, winsize: int=20, step: int = 3):
-    for i in range(0, len(sequence), step):
-        for j in ["A", "T", "C", "G"]:
 
-        num_G = count()
-        num_AT = 0
-        window = sequence[i:i+winsize]
+def remove_GCC(sequence: str, min_GC: float, max_GC: float) -> str:
+    """Calculates the GC content of the input string"""
+    num_G = sequence.count("G")
+    num_C = sequence.count("C")
+    num_A = sequence.count("A")
+    num_T = sequence.count("G")
 
+    num_GC = num_G + num_C
+    num_AT = num_A + num_T
+
+    GCC = num_GC / (num_GC+num_AT)
+
+    if not(min_GC < GCC < max_GC):
+        sequence = re.sub("[A-Z]", "-", sequence)
+
+    return sequence
 
 
 class Filters:
@@ -27,13 +38,7 @@ class Filters:
 
     def GC_content(self, max_GC, min_GC):
         """Removes portions of genome that have too much/little GC-content"""
-        for i in range(0, len(self.forward[0]), 3):
-            window = self.sequences[0]
-
-
-
-
-
+        pass
 
     def annealing_temp(self, max_T, min_T):
         """Only keep primers /w appropriate Anneal_temp """
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 
 from Fasta_DNA import Fasta_DNA
 from Trie import Trie
-from Filters import Filters
+from Filters import Filters, remove_GCC
 
 
 def get_config(config_file: str) -> dict:
@@ -25,11 +25,11 @@ def get_logger(log_file: str):
 
     file_handler = logging.FileHandler(log_file, mode="w")
     file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(logging.DEBUG)
 
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
-    stream_handler.setLevel(logging.DEBUG)
+    stream_handler.setLevel(logging.INFO)
 
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
@@ -44,23 +44,40 @@ def main():
     t = Trie()
     P2_genome = Fasta_DNA(config["files"]["P2"])
 
-    P2_sequences = [P2_genome.get_forward_strand(), P2_genome.get_reverse_strand()]
-
+    P2_f = P2_genome.get_forward_strand()
+    P2_rev = P2_genome.get_reverse_strand()
 
     PRIMER_LENGTH = config["settings"]["length"]
+    GC_MAX = config["settings"]["GC_max"]
+    GC_MIN = config["settings"]["GC_min"]
+
+    P2_flist = [P2_f[i:i+PRIMER_LENGTH] for i in range(0, len(P2_f), PRIMER_LENGTH)]
+
+
+    for i, primer in enumerate(P2_flist):
+        primer = remove_GCC(primer, GC_MIN, GC_MAX)
+
+        P2_flist[i] = primer
+
+        #logger.info(primer)
+
+
+    P2_f = "".join(P2_flist)
+
     for i in range(len(P2_f) - PRIMER_LENGTH-1):
         primer = P2_f[i: i + PRIMER_LENGTH]
-        t.insert(primer)
+        if not ("-" in primer):
+            t.insert(primer)
 
     for i in range(len(P2_rev) - PRIMER_LENGTH-1):
         primer = P2_rev[i:i + PRIMER_LENGTH]
-
-        t.insert(primer)
+        if not("-" in primer):
+            t.insert(primer)
 
     test = t.query("")
     for i in test:
         logger.info(f"{i[0]}, {i[1]}")
-
+    print(len(test))
 
 if __name__ == '__main__':
     main()
